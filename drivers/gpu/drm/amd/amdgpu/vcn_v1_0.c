3ee369605a2124dbff7626003a736f5c68445349
Boyuan Zhang <boyuan.zhang@amd.com>
2024-10-07 13:35:33 -0400

    drm/amdgpu: move per inst variables to amdgpu_vcn_inst
    BugLink: https://bugs.launchpad.net/bugs/2091856
    
    Move all per instance variables from amdgpu_vcn to amdgpu_vcn_inst.
    
    Move adev->vcn.fw[i] from amdgpu_vcn to amdgpu_vcn_inst.
    Move adev->vcn.vcn_config[i] from amdgpu_vcn to amdgpu_vcn_inst.
    Move adev->vcn.vcn_codec_disable_mask[i] from amdgpu_vcn to amdgpu_vcn_inst.
    
    Signed-off-by: Boyuan Zhang <boyuan.zhang@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit cf1aa9ffd42f1c26706f6c705c9dde63a0f44da8)
    Signed-off-by: Yo-Jung (Leo) Lin <leo.lin@canonical.com>
    Acked-by: Chia-Lin Kao (AceLan) <chia-lin.kao@canonical.com>
    Signed-off-by: Kuan-Ying Lee <kuan-ying.lee@canonical.com>

--- drivers/gpu/drm/amd/amdgpu/vcn_v1_0.c
+++ drivers/gpu/drm/amd/amdgpu/vcn_v1_0.c
@@ -297,7 +297,7 @@ static int vcn_v1_0_resume(void *handle)
  */
 static void vcn_v1_0_mc_resume_spg_mode(struct amdgpu_device *adev)
 {
-	uint32_t size = AMDGPU_GPU_PAGE_ALIGN(adev->vcn.fw[0]->size + 4);
+	uint32_t size = AMDGPU_GPU_PAGE_ALIGN(adev->vcn.inst[0].fw->size + 4);
 	uint32_t offset;
 
 	/* cache window 0: fw */
@@ -364,7 +364,7 @@ static void vcn_v1_0_mc_resume_spg_mode(struct amdgpu_device *adev)
 
 static void vcn_v1_0_mc_resume_dpg_mode(struct amdgpu_device *adev)
 {
-	uint32_t size = AMDGPU_GPU_PAGE_ALIGN(adev->vcn.fw[0]->size + 4);
+	uint32_t size = AMDGPU_GPU_PAGE_ALIGN(adev->vcn.inst[0].fw->size + 4);
 	uint32_t offset;
 
 	/* cache window 0: fw */
