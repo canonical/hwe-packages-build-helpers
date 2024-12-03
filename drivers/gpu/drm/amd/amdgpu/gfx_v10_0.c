1218f966d0712c79445680fb07c29f7c5c1f02da
Mario Limonciello <mario.limonciello@amd.com>
2024-12-03 11:28:54 -0600

    drm/amd: Add the capability to mark certain firmware as "required"
    BugLink: https://bugs.launchpad.net/bugs/2091856
    
    Some of the firmware that is loaded by amdgpu is not actually required.
    For example the ISP firmware on some SoCs is optional, and if it's not
    present the ISP IP block just won't be initialized.
    
    The firmware loader core however will show a warning when this happens
    like this:
    ```
    Direct firmware load for amdgpu/isp_4_1_0.bin failed with error -2
    ```
    
    To avoid confusion for non-required firmware, adjust the amd-ucode helper
    to take an extra argument indicating if the firmware is required or
    optional.
    
    On optional firmware use firmware_request_nowarn() instead of
    request_firmware() to avoid the warnings.
    
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Link: https://lore.kernel.org/amd-gfx/df71d375-7abd-4b32-97ce-15e57846eed8@amd.com/T/#t
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit ea5d49349894a7a74ce8dba242e3a487d24b6c0e linux-next)
    Signed-off-by: Yo-Jung (Leo) Lin <leo.lin@canonical.com>
    Acked-by: Chia-Lin Kao (AceLan) <chia-lin.kao@canonical.com>
    Signed-off-by: Kuan-Ying Lee <kuan-ying.lee@canonical.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v10_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v10_0.c
@@ -4132,18 +4132,21 @@ static int gfx_v10_0_init_microcode(struct amdgpu_device *adev)
 	amdgpu_ucode_ip_version_decode(adev, GC_HWIP, ucode_prefix, sizeof(ucode_prefix));
 
 	err = amdgpu_ucode_request(adev, &adev->gfx.pfp_fw,
+				   AMDGPU_UCODE_REQUIRED,
 				   "amdgpu/%s_pfp%s.bin", ucode_prefix, wks);
 	if (err)
 		goto out;
 	amdgpu_gfx_cp_init_microcode(adev, AMDGPU_UCODE_ID_CP_PFP);
 
 	err = amdgpu_ucode_request(adev, &adev->gfx.me_fw,
+				   AMDGPU_UCODE_REQUIRED,
 				   "amdgpu/%s_me%s.bin", ucode_prefix, wks);
 	if (err)
 		goto out;
 	amdgpu_gfx_cp_init_microcode(adev, AMDGPU_UCODE_ID_CP_ME);
 
 	err = amdgpu_ucode_request(adev, &adev->gfx.ce_fw,
+				   AMDGPU_UCODE_REQUIRED,
 				   "amdgpu/%s_ce%s.bin", ucode_prefix, wks);
 	if (err)
 		goto out;
@@ -4167,6 +4170,7 @@ static int gfx_v10_0_init_microcode(struct amdgpu_device *adev)
 	}
 
 	err = amdgpu_ucode_request(adev, &adev->gfx.mec_fw,
+				   AMDGPU_UCODE_REQUIRED,
 				   "amdgpu/%s_mec%s.bin", ucode_prefix, wks);
 	if (err)
 		goto out;
@@ -4174,6 +4178,7 @@ static int gfx_v10_0_init_microcode(struct amdgpu_device *adev)
 	amdgpu_gfx_cp_init_microcode(adev, AMDGPU_UCODE_ID_CP_MEC1_JT);
 
 	err = amdgpu_ucode_request(adev, &adev->gfx.mec2_fw,
+				   AMDGPU_UCODE_REQUIRED,
 				   "amdgpu/%s_mec2%s.bin", ucode_prefix, wks);
 	if (!err) {
 		amdgpu_gfx_cp_init_microcode(adev, AMDGPU_UCODE_ID_CP_MEC2);
