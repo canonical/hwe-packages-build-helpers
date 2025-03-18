c469fe9bb6df21aad992fb2e8cb6ec50bd06b026
En-Wei Wu <en-wei.wu@canonical.com>
2025-03-18 18:38:00 +0800

    drm/amdgpu: update the handle ptr in suspend
    BugLink: https://bugs.launchpad.net/bugs/2103480
    
    Update the *handle to amdgpu_ip_block ptr for all
    functions pointers of suspend.
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (backported from commit 982d7f9bfe4a50334543ef97808615693d7ac29e linux-next)
    [En-Wei: Remove the XGMI codes which doesn't match with our OEM kernel]
    Signed-off-by: En-Wei Wu <en-wei.wu@canonical.com>
    Acked-by: Chia-Lin Kao (AceLan) <chia-lin.kao@canonical.com>
    Signed-off-by: Kuan-Ying Lee <kuan-ying.lee@canonical.com>

--- drivers/gpu/drm/amd/pm/legacy-dpm/kv_dpm.c
+++ drivers/gpu/drm/amd/pm/legacy-dpm/kv_dpm.c
@@ -3066,9 +3066,9 @@ static int kv_dpm_hw_fini(void *handle)
 	return 0;
 }
 
-static int kv_dpm_suspend(void *handle)
+static int kv_dpm_suspend(struct amdgpu_ip_block *ip_block)
 {
-	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
+	struct amdgpu_device *adev = ip_block->adev;
 
 	cancel_work_sync(&adev->pm.dpm.thermal.work);
 
--- drivers/gpu/drm/amd/pm/legacy-dpm/si_dpm.c
+++ drivers/gpu/drm/amd/pm/legacy-dpm/si_dpm.c
@@ -7806,9 +7806,9 @@ static int si_dpm_hw_fini(void *handle)
 	return 0;
 }
 
-static int si_dpm_suspend(void *handle)
+static int si_dpm_suspend(struct amdgpu_ip_block *ip_block)
 {
-	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
+	struct amdgpu_device *adev = ip_block->adev;
 
 	cancel_work_sync(&adev->pm.dpm.thermal.work);
 
