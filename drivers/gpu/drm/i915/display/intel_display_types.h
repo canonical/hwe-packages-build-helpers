e32dcb9a296934433a7a30afd87dd729cb5f906f
Jouni Högander <jouni.hogander@intel.com>
2025-03-28 10:06:23 +0200

    drm/i915/psr: Add PSR pause/resume reference count
    BugLink: https://bugs.launchpad.net/bugs/2130998
    
    We have now seen this:
    
    <4> [2120.434153] i915 0000:00:02.0: [drm] drm_WARN_ON(psr->paused)
    <4> [2120.434196] WARNING: CPU: 3 PID: 4430 at drivers/gpu/drm/i915/display/intel_psr.c:2227 intel_psr_pause+0x16e/0x180 [i915]
    
    Comment for drm_WARN_ON(display->drm, psr->paused) in intel_psr_pause says:
    
    "If we ever hit this, we will need to add refcount to pause/resume"
    
    This patch is implementing PSR pause/resume refcount.
    
    v3: Incorporate changes missing from v2
    v2: Add drm_warn for detecting possible unbalanced pause/resume
    
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>
    Reviewed-by: Animesh Manna <animesh.manna@intel.com>
    Link: https://lore.kernel.org/r/20250328080623.1183669-1-jouni.hogander@intel.com
    (cherry picked from commit 9900e35a3ab14e1dcd5987b7ea13150e9ba2dc02)
    Signed-off-by: Chia-Lin Kao (AceLan) <acelan.kao@canonical.com>
    Acked-by: You-Sheng Yang <vicamo.yang@canonical.com>
    Signed-off-by: Chia-Lin Kao (AceLan) <chia-lin.kao@canonical.com>

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -1609,7 +1609,7 @@ struct intel_psr {
 	bool sink_support;
 	bool source_support;
 	bool enabled;
-	bool paused;
+	int pause_counter;
 	enum pipe pipe;
 	enum transcoder transcoder;
 	bool active;
