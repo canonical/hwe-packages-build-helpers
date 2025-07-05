aef27942ae572fa02b25d62f7c1e653e73a7c650
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2025-07-05 11:52:33 +0200

    staging: gpib: Fix error handling paths in cb_gpib_probe()
    BugLink: https://bugs.launchpad.net/bugs/2126463
    
    [ Upstream commit 1b0ee85ee7967a4d7a68080c3f6a66af69e4e0b4 ]
    
    If cb_gpib_config() fails, 'info' needs to be freed, as already done in the
    remove function.
    
    While at it, remove a pointless comment related to gpib_attach().
    
    Fixes: e9dc69956d4d ("staging: gpib: Add Computer Boards GPIB driver")
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Link: https://lore.kernel.org/r/bf89d6f2f8b8c680720d02061fc4ebdd805deca8.1751709098.git.christophe.jaillet@wanadoo.fr
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- drivers/staging/gpib/cb7210/cb7210.c
+++ drivers/staging/gpib/cb7210/cb7210.c
@@ -1194,8 +1194,7 @@ struct local_info {
 static int cb_gpib_probe(struct pcmcia_device *link)
 {
 	struct local_info *info;
-
-//	int ret, i;
+	int ret;
 
 	DEBUG(0, "%s(0x%p)\n", __func__, link);
 
@@ -1223,8 +1222,16 @@ static int cb_gpib_probe(struct pcmcia_device *link)
 
 	/* Register with Card Services */
 	curr_dev = link;
-	return cb_gpib_config(link);
-} /* gpib_attach */
+	ret = cb_gpib_config(link);
+	if (ret)
+		goto free_info;
+
+	return 0;
+
+free_info:
+	kfree(info);
+	return ret;
+}
 
 /*
  *   This deletes a driver "instance".  The device is de-registered
