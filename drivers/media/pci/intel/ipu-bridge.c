a76d94e62acf661f064b7c3aba5b9a102b684f76
Jimmy Su <jimmy.su@intel.com>
2025-08-25 22:45:22 +0800

    UBUNTU: SAUCE: media: ipu-bridge: Support s5k3j1 sensor
    BugLink: https://bugs.launchpad.net/bugs/2121852
    
    (cherry picked from commit https://github.com/intel/ipu6-drivers/blob/0d976128ec46ee8abc774883375315dcc74cae9e/patch/v6.14/0023-media-ipu-bridge-Support-s5k3j1-sensor.patch)
    Signed-off-by: You-Sheng Yang <vicamo.yang@canonical.com>

--- drivers/media/pci/intel/ipu-bridge.c
+++ drivers/media/pci/intel/ipu-bridge.c
@@ -58,6 +58,8 @@ static const struct ipu_sensor_config ipu_supported_sensors[] = {
 	IPU_SENSOR_CONFIG("INT0310", 0),
 	/* Omnivision OV5693 */
 	IPU_SENSOR_CONFIG("INT33BE", 1, 419200000),
+	/* Samsung S5K3J1 */
+	IPU_SENSOR_CONFIG("INT346D", 1, 848000000),
 	/* Omnivision OV2740 */
 	IPU_SENSOR_CONFIG("INT3474", 1, 180000000),
 	/* Omnivision OV8865 */
