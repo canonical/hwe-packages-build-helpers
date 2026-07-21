fc53f361f086c6ba3b83171ea3a2eb5a53e8aab3
Ricardo Robaina <rrobaina@redhat.com>
2026-07-20 20:44:47 -0400

    audit: add audit_log_nf_skb helper function
    BugLink: https://bugs.launchpad.net/bugs/2166995
    
    [ Upstream commit f19590b07cb620be1fcd5474c49515e21a05d406 ]
    
    Netfilter code (net/netfilter/nft_log.c and net/netfilter/xt_AUDIT.c)
    have to be kept in sync. Both source files had duplicated versions of
    audit_ip4() and audit_ip6() functions, which can result in lack of
    consistency and/or duplicated work.
    
    This patch adds a helper function in audit.c that can be called by
    netfilter code commonly, aiming to improve maintainability and
    consistency.
    
    Suggested-by: Florian Westphal <fw@strlen.de>
    Suggested-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Ricardo Robaina <rrobaina@redhat.com>
    Acked-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Stable-dep-of: 65dfde57d1e2 ("audit: fix potential integer overflow in audit_log_n_hex()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [noble: adjust context]
    Signed-off-by: Necromancer <kernel-team@lists.ubuntu.com>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- include/linux/audit.h
+++ include/linux/audit.h
@@ -186,6 +186,8 @@ extern int audit_log_subject_context(struct audit_buffer *ab,
 				     struct lsmblob *blob);
 extern int audit_log_task_context(struct audit_buffer *ab);
 extern void audit_log_task_info(struct audit_buffer *ab);
+extern int audit_log_nf_skb(struct audit_buffer *ab,
+			    const struct sk_buff *skb, u8 nfproto);
 
 extern int		    audit_update_lsm_rules(void);
 
@@ -258,6 +260,12 @@ static inline int audit_log_task_context(struct audit_buffer *ab)
 }
 static inline void audit_log_task_info(struct audit_buffer *ab)
 { }
+
+static inline int audit_log_nf_skb(struct audit_buffer *ab,
+				   const struct sk_buff *skb, u8 nfproto)
+{
+	return 0;
+}
 
 static inline kuid_t audit_get_loginuid(struct task_struct *tsk)
 {
